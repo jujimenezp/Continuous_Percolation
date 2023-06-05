4b59890e2ad7d66f43486a874206634f6cc65c47
jujimenezp <jujimenezp@unal.edu.co>
2023-06-04 21:55:40 -0500

    Created class Percolacion for the functions used.

--- perco.cpp
+++ perco.cpp
@@ -4,12 +4,11 @@ int main(int argc, char** argv){
     const int t_end=stoi(argv[2]);
     const double R=3;
     const int L=50;
-    const int L2=L*L;
     Crandom ran64(stoi(argv[1]));
-    std::vector<double> b;
+    Percolacion perc(L,R,t_end);
 
-    b = create_system(ran64, L, t_end);
-    print_system("results.dat", b);
-    double x = find_t_cluster(b,t_end,R);
+    perc.create_system(ran64);
+    perc.find_t_cluster();
+    perc.print_system("results.dat");
     return 0;
 }
--- perco.hpp
+++ perco.hpp
@@ -7,30 +7,45 @@
 #ifndef PERCO_H_
 #define PERCO_H_
 
-std::vector<double> create_system(Crandom &ran64, int L, int t){
+class Percolacion{
+    private:
+        const int L, L2, t_end;
+        const double R;
+        std::vector<double> b;
+        std::vector<int> clusters;
+    public:
+        Percolacion(const int Li, const int t_endi, const double Ri) :
+            L(Li), L2(L*L), t_end(t_endi), R(Ri) {}
+        void create_system(Crandom &ran64);
+        void print_system(std::string filename);
+        void find_t_cluster();
+};
+
+void  Percolacion::create_system(Crandom &ran64){
     //b will save x(even),y(odd) coordinates for every circle
-    std::vector<double> b;
-    for(int i=0; i < t; i++){
+    for(int i=0; i < t_end; i++){
         b.push_back(L*ran64.r()); //x
         b.push_back(L*ran64.r()); //y
     }
-    return b;
+    clusters.resize(b.size()/2,0);
 }
 
-void print_system(std::string filename, std::vector<double> &b){
+void Percolacion::print_system(std::string filename){
     std::ofstream file(filename);
-    file << "x\t" <<"y\t" <<std::endl;
+    file << "x\t" <<"y\t" << "cluster" <<std::endl;
     for(int i=0; i < b.size(); i+=2){
-        file << b[i] << "\t" << b[i+1] <<std::endl;
+        file << b[i] << "\t" << b[i+1] <<"\t"<<clusters[i/2]<<std::endl;
     }
     file.close();
 }
 
-double find_t_cluster(std::vector<double> &b, int t_end, double R){
-    double t=t_end/2;
-    std::vector<int> clusters(b.size()/2,0);
+void Percolacion::find_t_cluster(){
+    //double t=t_end/2;
+    std::vector<bool> check_up(clusters.size(),false);
+    std::vector<bool> check_down(clusters.size(),false);
     clusters[0]=1;
     double s,x1,y1,x2,y2,aux=2;
+    int R2 = 2*R;
     for(int i=0; i < clusters.size(); i++){
         x1=b[2*i], y1=b[2*i+1];
         if(clusters[i]==0){
@@ -40,14 +55,17 @@ double find_t_cluster(std::vector<double> &b, int t_end, double R){
         for(int j=i+1;j < clusters.size(); j++){
             x2=b[2*j], y2=b[2*j+1];
             s = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-            if(s<=2*R && clusters[j]==0) clusters[j]=clusters[i];
-            else if(s<=2*R && clusters[j]<clusters[i]) clusters[i]=clusters[j];
+            if(s <= R2){
+                check_up[clusters[i]]=check_up[clusters[j]]=check_up[clusters[i]] || check_up[clusters[j]];
+                check_down[clusters[i]]=check_down[clusters[j]]=check_down[clusters[i]] || check_down[clusters[j]];
+                if(clusters[j]==0) clusters[j]=clusters[i];
+                else if(clusters[j]<clusters[i]) clusters[i]=clusters[j];
+            }
         }
     }
     for(auto const &i: clusters){
         std::cout << i << std::endl;
     }
-    return 5;
 }
 
 #endif // PERCO_H_
