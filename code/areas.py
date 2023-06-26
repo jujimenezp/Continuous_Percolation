a4a41fe9a782a45e6e5245b46822b68547bf677a
jujimenezp <jujimenezp@unal.edu.co>
2023-06-26 13:37:59 -0500

    Many Ls implemented

--- code/areas.py
+++ code/areas.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('results/areas_50.dat',delimiter='\t')
+data = pd.read_csv('results/areas_135.dat',delimiter='\t')
 x = data.iloc[:,1]
 bins = 30
 
 fig,ax = plt.subplots()
 ax.set_title(r'Área ocupada en $t_{percolante}$ para $L=50$')
 ax.hist(x, bins)
-#plt.show()
-plt.savefig('results/hist_50.pdf',format='pdf',bbox_inches='tight')
+plt.show()
+#plt.savefig('results/hist_50.pdf',format='pdf',bbox_inches='tight')
--- code/perco.cpp
+++ code/perco.cpp
@@ -3,48 +3,55 @@
 
 int main(int argc, char** argv){
     const int t_end=stoi(argv[2]);
-    const double R=4;
-    const int L=50;
-    const int N=2000;
+    const double R=3;
+    //const int L=135;
+    int Ls[] = {45, 60, 75, 90, 120, 135};
+    //int Ls[] = {120};
+    const int Ns[] = {1800, 2000, 5000, 6000, 1000, 18000};
     const int seeds = stoi(argv[1]);
     int t_perc, counter;
     double prop;
-    Crandom ran64(1);
+    Crandom *ran64;
     //Percolacion perc(L,t_end,R);
     Percolacion *perc;
     Monte_Carlo_Integral *Area;
     //Monte_Carlo_Integral Area(L,R,N);
 
-    std::ofstream areas("results/areas_"+to_string(L)+".dat");
-    areas << "seed" <<"\t"<<"Area"<<std::endl;
+    for (int i=0; i < sizeof(Ls)/sizeof(Ls[0]); i++){
+        std::cout << "L=" << Ls[i] << std::endl;
 
-    for(int s=0;s < seeds; s++){
-        std::cout << "Semilla: "<< s <<"\r";
-        ran64 = Crandom(s);
-        counter = 0;
-        perc = new Percolacion(L,t_end,R);
-        Area = new Monte_Carlo_Integral(L,R,N);
+        std::ofstream areas("results/areas_"+to_string(Ls[i])+".dat");
+        areas << "seed" <<"\t"<<"Area"<<std::endl;
+        for(int s=0;s < seeds; s++){
+            std::cout << "Semilla: "<< s <<"\r";
 
-        perc->create_system(ran64);
-        t_perc=perc->t_percolante();
-        //std::cout << "Cluster percolante en t=" <<t_perc <<std::endl;
+            perc = new Percolacion(Ls[i],t_end,R);
+            Area = new Monte_Carlo_Integral(Ls[i],R,Ns[i]);
+            ran64 = new Crandom(s);
+
+            perc->create_system(*ran64);
+            t_perc=perc->t_percolante();
+            //std::cout << "Cluster percolante en t=" <<t_perc <<std::endl;
 
 
-        counter = Area->mci(perc->get_b(), t_perc);
-        prop = double(counter)/double(N);
+            counter = Area->mci(perc->get_b(), t_perc);
+            prop = double(counter)/double(Ns[i]);
 
-        // std::cout << "phi_eff=" << prop << "\n"
-        //           << "Area=" << prop*L*L << "\n";
+            // std::cout << "phi_eff=" << prop << "\n"
+            //           << "Area=" << prop*Ls[i]*Ls[i] << "\n";
 
-        areas << s <<"\t" << prop*L*L <<std::endl;
+            areas << s <<"\t" << prop*Ls[i]*Ls[i] <<std::endl;
 
-        delete Area;
-        delete perc;
+            //perc->print_system("results/results_"+to_string(Ls[i])+".dat", t_perc);
+            Area->print(perc->get_b(),t_perc,"results/mci_circles.dat","results/mci_points.dat");
+
+            delete ran64;
+            delete Area;
+            delete perc;
+        }
+
+        areas.close();
     }
-
-    //perc->print_system("results/results_"+to_string(L)+".dat", t_perc);
-    //Area->print(perc->get_b(),t_perc,"results/mci_circles.dat","results/mci_points.dat");
-    areas.close();
 
     return 0;
 }
